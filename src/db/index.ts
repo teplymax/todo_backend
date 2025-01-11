d2c1e736bd9cfb5e9db83e9767b30a500addd43d
Maksym Teplyi <teplymax@gmail.com>
2025-01-11 17:53:22 +0200

    MT | Establish CD pipeline

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,20 +2,25 @@ import path from "path";
 
 import { DataSource } from "typeorm";
 
-import config from "@config";
+import config, { IS_PROD } from "@config";
 
 async function initializeDatabase() {
   const ext = path.extname(import.meta.url);
+  const rootDir = IS_PROD ? "src/dist" : "src";
 
   const db = new DataSource({
     ...config.db,
     type: "postgres",
-    entities: [`src/db/entities/*${ext}`],
+    entities: [`${rootDir}/db/entities/*${ext}`],
     logging: false,
     synchronize: true
   });
 
-  return await db.initialize();
+  const instance = await db.initialize();
+
+  console.log("DB initialized!");
+
+  return instance;
 }
 
 const db = await initializeDatabase();
