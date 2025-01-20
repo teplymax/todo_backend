9b9d285e61b8b1e18568aa567d22b0a3d0b6fcd4
Maksym Teplyi <teplymax@gmail.com>
2025-01-20 12:13:27 +0200

    MT | Fix rotes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 
 import authRouter from "./auth.router";
 import categoryRouter from "./category.router";
+import todoRouter from "./todo.router";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/auth", authRouter);
 
 apiRouter.use("/category", categoryRouter);
+
+apiRouter.use("/todo", todoRouter);
 
 export default apiRouter;
